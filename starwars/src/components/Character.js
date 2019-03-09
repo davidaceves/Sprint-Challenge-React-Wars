103987634d5fb5e3c7939be0a2cf695f5bd3f38b
David <digital0david@gmail.com>
2019-03-09 14:39:44 -0500

    css

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -5,34 +5,40 @@ function Character(props) {
     return (
         <div className="Character"> 
             <h2>Name: {props.char.name}</h2>
-            <p>Birth: {props.char.birth_year}</p>
-            <p>Species: {props.char.species}</p>
-            <p>Height: {props.char.height}</p>
-            <p>Gender: {props.char.gender}</p>
-            <p>Mass: {props.char.eye_color}</p>
-            <p>Hair Color: {props.char.hair_color}</p>
-            <p>Homeworld: {props.char.homeworld}</p>
+            <p><span>Birth:</span> {props.char.birth_year}</p>
+            <p><span>Species:</span> {props.char.species}</p>
+            <p><span>Height:</span> {props.char.height}</p>
+            <p><span>Gender:</span> {props.char.gender}</p>
+            <p><span>Mass:</span> {props.char.eye_color}</p>
+            <p><span>Hair Color:</span> {props.char.hair_color}</p>
+            <p><span>Homeworld:</span> {props.char.homeworld}</p>
 
-            {/* <h3>Starships</h3>
-            {props.char.starships.map(ship => {
-                return <li>{ ship }</li>;
-            })} */}
+            <h3>Starships:</h3>
+            <div className="list-container">
+                {props.char.starships.map(ship => {
+                    return <li>{ ship }</li>;
+                })}
+            </div>
 
-            <h3>Starships</h3>
-            {props.char.starships.map(ship => {
-                return <li>{ ship }</li>;
-            })}
+            <h3>Vehicles:</h3>
+            <div className="list-container"> 
+                {props.char.vehicles.map(vehicle => {
+                    return <li>{ vehicle }</li>;
+                })}
+            </div>
 
-            <h3>Vehicles</h3>
-            {props.char.vehicles.map(vehicle => {
-                return <li>{ vehicle }</li>;
-            })}
-
-            
             <h3>Films:</h3>
-            {props.char.films.map(film => {
-                return <li>{ film }</li>;
-            })}
+            <div className="list-container"> 
+                {props.char.films.map(film => {
+                    return <li>{ film }</li>;
+                })}
+            </div>
+
+            <h3>Created:</h3>
+            <p>{props.char.created}</p>
+
+            <h3>Edited:</h3>
+            <p>{props.char.edited}</p>
             
 
 
